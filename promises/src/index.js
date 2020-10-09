f6610406e7e5e1cd2ba5cec78dbea642a8c31168
MiltonMora <mayloevan@gmail.com>
2020-10-09 17:28:44 +0200

    with promises

---
+++ promises/src/index.js
@@ -0,0 +1,19 @@
+const fetchData = require('../utils/fetch');
+const fectcData = require('../utils/fetch');
+const API = 'https://rickandmortyapi.com/api/character/'
+
+fetchData(API)
+.then(all => {
+    console.log(all.info.count)
+    return fetchData(`${API}${all.results[0].id}`)
+})
+.then(character => {
+    console.log(character.name)
+    return fetchData(character.origin.url)
+})
+.then(location => {
+    console.log(location.name)
+    console.log(location.type)
+    console.log(location.dimension)
+})
+.catch((error) => console.log(error));
